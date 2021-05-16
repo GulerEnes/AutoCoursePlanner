60a51dabcaca68f626a4c7ce362bc1b40b896ec4
GulerEnes <kriptoenes@gmail.com>
2021-05-16 23:27:12 +0300

    BİTTTİİ

--- ProjectObjects.py
+++ ProjectObjects.py
@@ -1,93 +1,59 @@
-class Classroom:
-    numOfClassroom = 0
-    bigClassAmount = 0
-    smallClassAmount = 0
-
-    def __init__(self, type, amount):
-        self.type = type
-        self.amount = amount
-        Classroom.numOfClassroom += int(self.amount)
-
-    def __repr__(self):
-            return f"""type: {self.type}
-amount: {self.amount}
--------------------------------------------------
-"""
-
 class Course:
-    numOfCourses = 0
+    numberOfCompulsoryCourses = 0
+    numberOfElectiveCourses = 0
 
     def __init__(self, code, name, year, credit, CE, DS, instructor):
         self.code = code
         self.name = name
-        self.year = year
-        self.credit = credit
-        self.CE = CE
-        self.DS = DS
+        self.year = int(year)
+        self.credit = int(credit)
+        self.CE = CE  # Compulsory - Elective
+        self.DS = DS  # Department - Service
         self.instructor = instructor
-        self.serviceTimeSlot_day = None
-        self.serviceTimeSlot_clock = None
-        Course.numOfCourses += 1
 
-    def __repr__(self):  # I wrote this for temporarily
-        return f"""code: {self.code}
-name: {self.name}
-year: {self.year}
-credit: {self.credit}
-CE: {self.CE}
-DS: {self.DS}
-instructor: {self.instructor}
-serviceTimeSlot_day: {self.serviceTimeSlot_day}
-serviceTimeSlot_clock: {self.serviceTimeSlot_clock}
--------------------------------------------------
-"""
+        if CE == 'C':
+            Course.numberOfCompulsoryCourses += 1
+        else:
+            Course.numberOfElectiveCourses += 1
+
+    def __repr__(self):
+        return self.code + "," + self.name + "," + str(self.year) + "," + \
+               str(self.credit) + "," + self.CE + "," + self.DS + "," + self.instructor
 
 
 class ServiceCourse:
-    numOfServiceCourses = 0
-
     def __init__(self, code, day, clock):
         self.code = code
         self.day = day
         self.clock = clock
-        ServiceCourse.numOfServiceCourses += 1
-
-    def __repr__(self):
-            return f"""code: {self.code}
-day: {self.day}
-clock: {self.clock}
--------------------------------------------------
-"""
 
 
-class BusyInstructor:
-    numOfBusyInstructor = 0
-
-    def __init__(self, name):
-        self.name = name
-        self.busyTimeSlots = []
-        BusyInstructor.numOfBusyInstructor += 1
-
-    def appendBusyTimeSlot(self, day, clock):
-        self.busyTimeSlots.append([day, clock])
-    def __repr__(self):
-            return f"""name: {self.name}
-BusyTimeSlots: {self.busyTimeSlots}
--------------------------------------------------
-"""
-
-class Cirriculum:
-
-    def __init__(self,day,clock,classroom,code):
+class Classroom:
+    def __init__(self, className, day, clock, code):
         self.day = day
         self.clock = clock
-        self.classroom = classroom
+        self.className = className
         self.code = code
 
     def __repr__(self):
-            return f"""Day: {self.day}
-Clock: {self.clock}
-Classrom: {self.classroom}
-Code: {self.code}            
--------------------------------------------------
-"""
+        return str(self.day) + " " + str(self.clock) + " " + self.className + " " + str(self.code)
+
+
+class BusyInstructor:
+    def __init__(self, name):
+        self.name = name
+        self.busyTimeSlots = []
+        self.availableTimeSlots = {"Monday": ["Morning", "Afternoon"],
+                                   "Tuesday": ["Morning", "Afternoon"],
+                                   "Wednesday": ["Morning", "Afternoon"],
+                                   "Thursday": ["Morning", "Afternoon"],
+                                   "Friday": ["Morning", "Afternoon"]}
+
+    def appendBusyTimeSlot(self, day, clock):
+        self.busyTimeSlots.append([day, clock])
+
+    def availableTimesCalculator(self, busyInstructors):
+        for day, clock in busyInstructors[self.name].busyTimeSlots:
+            self.availableTimeSlots[day].remove(clock)
+            if len(self.availableTimeSlots[day]) == 0:
+                self.availableTimeSlots.pop(day)
--- main.py
+++ main.py
@@ -1,125 +1,155 @@
-from ProjectObjects import *
 import csv
+from Objects import *
+import random
 
-# Reading number of classes
-#numOfClasses = dict()
-classrooms = []
-with open("Sample_files/classroom.csv", mode='r') as classroomFile:
-    csvReader = csv.reader(classroomFile, delimiter=';')
-    for row in csvReader:
-        classrooms.append(Classroom(row[0],row[1]))
-#print(classrooms)
-#print(Classroom.numOfClassroom)
-Classroom.bigClassAmount = classrooms[0].amount
-Classroom.smallClassAmount = classrooms[1].amount
-#print(Classroom.bigClassAmount)
-#print(Classroom.smallClassAmount)
 
-courses = []
-with open("Sample_files/Courses.csv", mode='r') as coursesFile:
-    csvReader = csv.reader(coursesFile, delimiter=';')
-    for row in csvReader:
-        courses.append(Course(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
-#print(courses)
+def timeGenerator():
+    return {"Monday": ["Morning", "Afternoon"],
+            "Tuesday": ["Morning", "Afternoon"],
+            "Wednesday": ["Morning", "Afternoon"],
+            "Thursday": ["Morning", "Afternoon"],
+            "Friday": ["Morning", "Afternoon"]}
 
-services = []
-with open("Sample_files/service.csv", mode='r') as serviceFile:
-    csvReader = csv.reader(serviceFile, delimiter=';')
-    for row in csvReader:
-        services.append(ServiceCourse(row[0], row[1], row[2]))
-#print(services)
 
-busyInstructors = dict()
-with open("Sample_files/busy.csv", mode='r') as busyFile:
-    csvReader = csv.reader(busyFile, delimiter=';')
-    for row in csvReader:
-        if row[0] not in busyInstructors:
-            i = BusyInstructor(row[0])
-            busyInstructors[row[0]] = i
-            BusyInstructor.appendBusyTimeSlot(i, row[1], row[2])
-            busyInstructors[row[0]] = i
-        else:
-            BusyInstructor.appendBusyTimeSlot(busyInstructors[row[0]], row[1], row[2])
+def isNumberOfClassesEnough(bigClasses, smallClasses):
+    return len(bigClasses) * 10 > Course.numberOfCompulsoryCourses and len(smallClasses) * 10 + (
+            len(bigClasses) * 10 - Course.numberOfCompulsoryCourses) > Course.numberOfElectiveCourses
 
-#print(busyInstructors)
 
-#We combine the cources and services objects
-for course in courses:
+def findCorrectPlace(classes, courses, service, timesForYears):
+    for _class in classes:  # To find correct place
+        if _class.className[0] == 'b' and _class.code is None:
+            if _class.day == service.day and _class.clock == service.clock:
+                _class.code = service.code
+
+                timesForYears[courses[service.code].year - 1][_class.day].remove(_class.clock)
+                if 0 == len(timesForYears[courses[service.code].year - 1][_class.day]):
+                    timesForYears[courses[service.code].year - 1].pop(_class.day)
+                courses.pop(service.code)
+                break
+
+
+def putCourses(busyInstructors, courses, timesForYears, flag):
+    Ccount = 0
+    while len(courses.keys()) > 0 and Ccount < len(courses.keys()) * 20:
+        Ccount += 1
+        randomCourse = random.choice(list(courses.values()))
+        count = 0
+        while count < len(classes) * 20:
+            randomClassroom = random.choice(classes)
+            # Zorunlu ders, büyük sınıf ve bahsi geçen sınıfın o sırada boş olma durumu kontroü
+            if (flag == 'E' and randomClassroom.code is None) or (
+                    flag == 'C' and randomCourse.CE == 'C' and randomClassroom.className[0] == 'b'):
+                # Aynı yıla ait olan derslerin aynı satırda olmaması için zaman kontrolü
+
+                if randomClassroom.day in timesForYears[randomCourse.year - 1].keys() and randomClassroom.clock in \
+                        timesForYears[randomCourse.year - 1][randomClassroom.day]:
+
+                    # Hocanın müsaitlik kontrolü
+                    if randomCourse.instructor in busyInstructors.keys():
+                        availableTimes = busyInstructors[randomCourse.instructor].availableTimeSlots
+                        if randomClassroom.day in availableTimes.keys() and randomClassroom.clock in availableTimes[
+                            randomClassroom.day]:
+                            # Put the fuckin course here
+
+                            randomClassroom.code = randomCourse.code
+                            # Deleting time line from timesForYears
+                            timesForYears[randomCourse.year - 1][randomClassroom.day].remove(randomClassroom.clock)
+                            if 0 == len(timesForYears[randomCourse.year - 1][randomClassroom.day]):
+                                timesForYears[randomCourse.year - 1].pop(randomClassroom.day)
+                            courses.pop(randomCourse.code)  # Deleting course from courses
+                            break
+
+                    else:  # Hoca her zaman müsait
+                        randomClassroom.code = randomCourse.code
+                        # Deleting time line from timesForYears
+                        timesForYears[randomCourse.year - 1][randomClassroom.day].remove(randomClassroom.clock)
+                        if 0 == len(timesForYears[randomCourse.year - 1][randomClassroom.day]):
+                            timesForYears[randomCourse.year - 1].pop(randomClassroom.day)
+                        courses.pop(randomCourse.code)  # Deleting course from courses
+                        break
+            count += 1
+
+
+maxTableTryNumber = 0
+
+
+def changingClassNumbers(oldBig, oldSmall):
+    with open('classroom.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow('Big;' + str(oldBig + 1))
+        writer.writerow('Small;' + str(oldSmall + 1))
+
+
+while maxTableTryNumber < 1000:
+    maxTableTryNumber += 1
+    # Hocacıklarımızın müsait olmadıkları zamanlar
+    busyInstructors = dict()
+    with open("busy.csv", mode='r') as busyFile:
+        csvReader = csv.reader(busyFile, delimiter=';')
+        for row in csvReader:
+            if row[0] not in busyInstructors:
+                busyInstructors[row[0]] = BusyInstructor(row[0])
+            busyInstructors[row[0]].appendBusyTimeSlot(row[1], row[2])
+
+    # Sınıf sayılarının okunması ve sınıf isimlerinin oluşturulması
+    numOfClasses = dict()
+    with open("classroom.csv", mode='r') as classroomFile:
+        csvReader = csv.reader(classroomFile, delimiter=';')
+        for row in csvReader:
+            numOfClasses[row[0]] = int(row[1])
+
+    bigClasses = ["bigClass_" + str(i + 1) for i in range(numOfClasses["big"])]
+    smallClasses = ["smallClass_" + str(i + 1) for i in range(numOfClasses["small"])]
+
+    # Derslerin okunması ve hepsinin tek bir listeye atılması
+    courses = dict()
+    with open("Courses.csv", mode='r') as coursesFile:
+        csvReader = csv.reader(coursesFile, delimiter=';')
+        for row in csvReader:
+            courses[row[0]] = Course(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+
+    # Service derslerinin okunması
+    services = []
+    with open("service.csv", mode='r') as serviceFile:
+        csvReader = csv.reader(serviceFile, delimiter=';')
+        for row in csvReader:
+            services.append(ServiceCourse(row[0], row[1], row[2]))
+
+    classes = [Classroom(className, day, clock, None) for className in bigClasses + smallClasses
+               for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"] for clock in
+               ["Morning", "Afternoon"]]
+
+    timesForYears = [timeGenerator(), timeGenerator(), timeGenerator(), timeGenerator()]
+
+    # Service derslerinin yerleştirilmesi
     for service in services:
-        if course.code == service.code:
-            course.serviceTimeSlot_day = service.day
-            course.serviceTimeSlot_clock = service.clock
-#print(courses)
+        if courses[service.code].CE == 'C':  # Compulsory Service Course
+            findCorrectPlace(classes, courses, service, timesForYears)
+        else:  # Elective service course
+            findCorrectPlace(classes, courses, service, timesForYears)
+            if service.code in courses:  # Eğer uygun küçük sınıf yoksa büyük sınıfa koy
+                findCorrectPlace(classes, courses, service, timesForYears)
 
-years = [[]]*4
-print(years)
-for course in courses:
-    if int(course.year) == 1:
-        years[0].append(course)
-    elif int(course.year) == 2:
-        years[1].append(course)
-    elif int(course.year) == 3:
-        years[2].append(course)
-    else:
-        years[3].append(course)
-print(years[0])
+    # Hocacıklarımızın müsait olduğu zamanları tutabilmek için hesaplama yapıyoruz
+    for busyInstructor in busyInstructors.values():
+        busyInstructor.availableTimesCalculator(busyInstructors)
+
+    if isNumberOfClassesEnough(bigClasses, smallClasses):
+        putCourses(busyInstructors, courses, timesForYears, 'C')
+        putCourses(busyInstructors, courses, timesForYears, 'E')
 
 
+    else:  # Sınıf sayısı yetmiyor
+        # Sınıfsayısını artır ve bütün dosyayı en baştan çalıştır.
+        Course.numberOfCompulsoryCourses = 0
+        Course.numberOfElectiveCourses = 0
+        if maxTableTryNumber == 999:
+            maxTableTryNumber = 0
+            changingClassNumbers(numOfClasses["big"], numOfClasses["small"])
 
-"""cirriculum = []
-for service in services:
-    c = Cirriculum(service.day,service.clock,"null",service.code)
-    cirriculum.append(c)"""
-
-#print(cirriculum)
-
-
-"""
-daysDic = { 0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday"}
-clockDic = {0: "Morning", 1: "Afternoon"}
-
-daysDic2 = { "Monday": 0, "Tuesday": 1, "Wednesday": 2, "Thursday": 3, "Friday": 4}
-clockDic2 = {"Morning": 0, "Afternoon": 1}"""
+    if len(courses.keys()) == 0:
+        break
 
 
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-clock = ['Morning', 'Afternoon']
-
-i = 0
-j = 0
-matrix = []
-for i in range(len(days)):
-    for j in range(len(clock)):
-        matrix += [[days[i], clock[j]]]
-        #print(matrix)
-
-
-"""
-a = 0
-for service in services:
-    for a in range(len(days)*len(clock)):
-        print(a)
-        if matrix[a][0] == service.day and matrix[a][1] == service.clock:
-            #print(a)
-            matrix[a] = service
-            print(matrix[a])
-            break
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
-
-
-
-
+[print(i) for i in classes]
